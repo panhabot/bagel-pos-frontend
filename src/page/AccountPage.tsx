27a504168a1efef463e313f7e6cacd8c225bca52
panhabot <kongpanhabot2000@gmail.com>
2023-11-02 13:19:17 +0900

    WIP: account setting

--- src/page/AccountPage.tsx
+++ src/page/AccountPage.tsx
@@ -1,5 +1,137 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Label } from "recharts";
+import { z } from "zod";
 
 export const AccountPage: React.FC = () => {
-  return <div>AccountPage</div>;
+  const formSchema = z.object({
+    username: z.string().min(1),
+    email: z.string().email(),
+    dob: z.date({
+      required_error: "A date of birth is required.",
+    }),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <div className="space-y-6 p-4">
+      <div>
+        <h3 className="text-lg font-medium">Account</h3>
+        <p className="text-sm text-muted-foreground">
+          Update your account settings.
+        </p>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Avatar className="w-[128px] h-[128px] mb-4 flex items-center justify-center">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>KP</AvatarFallback>
+          </Avatar>
+          <div className="grid w-full max-w-sm items-center gap-2">
+            <Label>Picture</Label>
+            <Input id="picture" type="file" />
+          </div>
+          <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="UBagel" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="bagelpos@mail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="dob"
+              render={({ field }) => (
+                <FormItem className="flex flex-col row-start-2">
+                  <FormLabel>Date of birth</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button variant={"outline"}>
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button className="mt-4 bg-foreground text-white hover:text-black">
+            Update Account
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
